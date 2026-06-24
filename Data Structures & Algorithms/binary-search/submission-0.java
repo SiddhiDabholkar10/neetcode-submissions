00f4b85f86b634cdab1d0a6c042495661eb54f72
Siddhi Dabholkar <86103727+SiddhiDabholkar10@users.noreply.github.com>
2026-06-24 12:21:06 -0500

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int binarysearch(int[] nums, int target,int start, int end){
+        if(start>end) return -1;
+
+        int mid = (start+end)/2;
+        if(nums[mid] == target) return mid;
+        else if (nums[mid]<target){
+           return binarysearch(nums, target, mid+1,end);
+        }
+        else return binarysearch(nums, target, start, mid-1);
+    }
+    public int search(int[] nums, int target) {
+        
+       return binarysearch(nums,target,0, nums.length - 1);
+    }
+}
